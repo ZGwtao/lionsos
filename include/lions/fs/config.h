8c85dd4f2840ce76ede4c557c5ce02ca481e28c8
Zhu Guangtao <guangtaozhu@outlook.com>
2025-06-17 10:47:48 +1000

    fs: modify configs for introducing file system virtualiser
    Signed-off-by: Zhu Guangtao <guangtaozhu@outlook.com>

--- include/lions/fs/config.h
+++ include/lions/fs/config.h
@@ -7,11 +7,14 @@
 #include <stdint.h>
 #include <sddf/resources/common.h>
 
+#define LIONS_FS_MAX_CLIENTS 128
+
 #define LIONS_FS_MAGIC_LEN 8
 
 typedef struct fs_connection_resource {
     region_resource_t command_queue;
     region_resource_t completion_queue;
+    /* the shared region is for pathname transferring */
     region_resource_t share;
     uint16_t queue_len;
     uint8_t id;
@@ -19,10 +22,27 @@ typedef struct fs_connection_resource {
 
 typedef struct fs_server_config {
     char magic[LIONS_FS_MAGIC_LEN];
-    fs_connection_resource_t client;
+    /* the only client is the virtualiser */
+    fs_connection_resource_t virt;
 } fs_server_config_t;
 
 typedef struct fs_client_config {
     char magic[LIONS_FS_MAGIC_LEN];
-    fs_connection_resource_t server;
+    /* instead of server, connect to virtualiser for request dispatching and load-balancing */
+    fs_connection_resource_t virt;
+    /*
+     * the connection channel is for command routing/dispatching, while the shared data
+     * is for data I/O, which is needed by the block(storage driver)
+     *
+     * no need for a data region, because the client connects to the blk driver
+     * via shared-mem-based data region directly
+     */
 } fs_client_config_t;
+
+/* copy-paste from the block virtualiser */
+typedef struct fs_virt_config {
+    uint64_t num_clients;
+    char magic[LIONS_FS_MAGIC_LEN];
+    fs_server_config_t server;
+    fs_client_config_t clients[LIONS_FS_MAX_CLIENTS];
+} fs_virt_config_t;
