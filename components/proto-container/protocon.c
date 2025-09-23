398c09be7a0f7db2f1b6aac58d45530e92741d79
Zhu Guangtao <guangtaozhu@outlook.com>
2025-09-24 00:11:27 +1000

    update proto-container log
    Signed-off-by: Zhu Guangtao <guangtaozhu@outlook.com>

--- components/proto-container/protocon.c
+++ components/proto-container/protocon.c
@@ -52,7 +52,7 @@ void init(void)
 
     /* initialise the real trusted loader... */
     if (loader_context.flags.init != true) {
-        microkit_dbg_printf(PROGNAME "--> Init loader context\n");
+        microkit_dbg_printf(PROGNAME "Init loader context\n");
         tsldr_init(&loader_context, ed25519_verify, md->system_hash, sizeof(seL4_Word), 64);
         custom_memcpy(loader_context.public_key, md->public_key, sizeof(md->public_key));
         /* loader is now initialised... */
