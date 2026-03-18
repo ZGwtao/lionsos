08c4d6c8316bc07bbfb8a1962422db063d6758ad
Guangtao Zhu <guangtaozhu@outlook.com>
2026-03-18 11:58:17 +1100

    protocon: starting to migrate to Lionsos main branch
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

---
+++ examples/proto-container/copy2ramdisk.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Usage: ./copy-to-ramdisk.sh <file.elf> <partition_number>
+
+set -e
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <file.elf> <partition_number>"
+    exit 1
+fi
+
+FILE=$1
+PART=$2
+DISK=build/qemu_disk
+MOUNTPOINT=/mbp
+
+SECTOR_SIZE=512
+POFFSET=2048  # first partition starts at 2048
+
+# Get partition size (FS_COUNT) from fdisk output
+# Partition entries look like: build/qemu_disk1   *   2048  ...  <end>  <size>
+FS_COUNT=$(fdisk -l $DISK | awk -v d="$DISK" '$1 ~ d {print $4; exit}')
+
+if [ -z "$FS_COUNT" ]; then
+    echo "Failed to get partition size from fdisk"
+    exit 1
+fi
+
+# Compute offset: partition N starts at POFFSET + (N-1)*FS_COUNT
+OFFSET=$(( (POFFSET + (PART - 1) * FS_COUNT) * SECTOR_SIZE ))
+
+echo "Mounting partition $PART at offset=$OFFSET"
+sudo mount -o loop,offset=$OFFSET "$DISK" "$MOUNTPOINT"
+sudo cp "$FILE" "$MOUNTPOINT/"
+sudo umount "$MOUNTPOINT"
